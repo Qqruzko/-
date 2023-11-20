c6227b09e02c0c4d9015019bf44db1226d69c0bf
Qqruzko <150928737+Qqruzko@users.noreply.github.com>
2023-11-20 23:51:59 +0300

    Add files via upload

--- Algorithms/SQRT/SQRT/Program.cs
+++ Algorithms/SQRT/SQRT/Program.cs
@@ -13,9 +13,12 @@ namespace SQRT
            
             Console.WriteLine("Enter a number to find root");
             double target_x = double.Parse(Console.ReadLine());
-            Sqrt(target_x);
+            Sqrt(target_x, out double n);
+            Console.WriteLine(n);
+            Console.WriteLine(n * n);
+            Console.ReadLine();
         }
-        static void Sqrt(double target_x)
+        static void Sqrt(double target_x, out double n)
         {
             double oldx = 0;
             double x = 1;
@@ -23,12 +26,10 @@ namespace SQRT
             {
                 oldx = x;
                 x = (x + target_x / x) / 2;
-
+                n = x;
             }
             while (oldx != x);
-            Console.WriteLine(x);
-            Console.WriteLine(x * x);
-            Console.ReadLine();
+            
         }
     }
 }
