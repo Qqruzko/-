85b351511c50d9a489bc2b329b1b2dd28cb8d740
Qqruzko <150928737+Qqruzko@users.noreply.github.com>
2023-11-20 23:13:50 +0300

    Add files via upload

---
+++ Algorithms/SQRT/Loop demo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modu_2_
+{
+
+
+    class LoopProgram
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a number");
+            int number = int.Parse(Console.ReadLine());
+            GetIntArray(number); 
+            
+            int[] GetIntArray(int num)
+            {
+                List<int> listOfInts = new List<int>();
+                while (num > 0)
+                {
+                    listOfInts.Add(num % 10);
+                    num = num / 10;
+                }
+                listOfInts.Reverse();
+                int x = 0;
+                foreach (int element in listOfInts)
+                {
+                    x = x + element;
+                    Console.WriteLine(x);
+                }
+                return listOfInts.ToArray();
+
+                
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Algorithms/SQRT/SQRT/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQRT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Enter a number to find root");
+            double target_x = double.Parse(Console.ReadLine());
+            Sqrt(target_x);
+        }
+        static void Sqrt(double target_x)
+        {
+            double oldx = 0;
+            double x = 1;
+            do
+            {
+                oldx = x;
+                x = (x + target_x / x) / 2;
+
+            }
+            while (oldx != x);
+            Console.WriteLine(x);
+            Console.WriteLine(x * x);
+            Console.ReadLine();
+        }
+    }
+}
